8114b08613efc36985e1e149a1caa267673babc5
Rok Krulec <rok.krulec@gmail.com>
2014-11-26 17:51:23 -0500

    O shit, Haskell is LISP. Why didn't somebody say so.

---
+++ code/haskell/one_argument.hs
@@ -0,0 +1,25 @@
+-- every function has only one parameter
+a x = x + 3
+b = a 4
+
+-- if it has two, it gets partially executed with first parameter
+-- returns partially applied function
+-- which gets executed with second parameter
+f x y = x + y
+g = f 3
+h = g 4
+i = (f 4) 3
+
+-- f is basically a sum of two numbers function which is already available under name +
+j = (+ 4) 3
+
+-- if something is a function, it expects an argument on the right
+-- + takes 4 returns f
+-- f takes 3 returns 7
+-- + takes 7 returns f
+-- f takes 2 returns 9
+k = (+ ((+ 4) 3)) 2
+
+-- OMG, this is just a LISP
+l = (+ (+ 4) 3 ) 3 
+-- I'm going home
