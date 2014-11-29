a9be41de0cdcabe498793725e0798bfd453c7cbe
Rok Krulec <rok.krulec@gmail.com>
2014-11-29 12:16:32 -0500

    Towers of Hanoi implemented with let

--- code/haskell/towers_of_hanoi.hs
+++ code/haskell/towers_of_hanoi.hs
@@ -1,7 +1,20 @@
+-- Towers of Hanoi
+
 type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi 0 _ _ _ = []
 hanoi n a b c = hanoi (n-1) a c b ++ [(a, b)] ++ hanoi (n-1) c b a
+
+-- implementation with let  
+hanoi' :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi' 0 _ _ _ = []
+hanoi' n a b c = 
+    let
+        step1 = hanoi' (n-1) a c b
+        step2 = [(a, b)]
+        step3 = hanoi' (n-1) c b a
+    in
+        step1 ++ step2 ++ step3
 
