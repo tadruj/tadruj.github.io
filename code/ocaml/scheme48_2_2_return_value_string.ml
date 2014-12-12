4b5e265f23fdebafc9ab7f063aefcd6c2b267154
Rok Krulec <rok.krulec@gmail.com>
2014-12-12 13:24:02 -0500

    Scheme 48 phase 2.2 return value string

---
+++ code/ocaml/scheme48_2_2_return_value_string.ml
@@ -0,0 +1,31 @@
+(* install MParser with: opam install mparser *)
+#require "core"
+#require "mparser"
+open MParser
+open Core.Std
+
+let symbol = any_of "!#$%&|*+-/:<=>?@^_~"
+
+let spaces' = skip_many1 space
+
+type lispVal =
+	| Atom of string
+	| List of lispVal list
+	| DottedList of lispVal list * lispVal
+	| Number of int
+	| String of string
+	| Bool of bool
+
+let parseString =  MParser.char '"' 
+	>> (MParser.many (MParser.none_of "\"")) 
+	>>= (fun t -> MParser.char '"' >> return t)
+
+let parseExpr = parseString
+
+let readExpr input = match parse_string parseExpr input () with
+	| Success r -> "Found value: " ^ String.of_char_list r
+	| Failed (msg, e) -> "No match on: " ^ input ^ ": " ^ msg
+
+let () =
+	let args = Sys.argv in
+    print_string (readExpr args.(1) ^ "\n");
