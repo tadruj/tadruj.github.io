ddf7e051e445d65a9e1e41df23444e17d3fdae45
Rok Krulec <rok.krulec@gmail.com>
2014-12-16 10:01:51 -0500

    Recursion with lazy functions

--- code/ocaml/letrec.ml
+++ code/ocaml/letrec.ml
@@ -1,15 +1,17 @@
-open Lazy
+#require "core"
+open Core.Std
 
 let rec 
-	a (c) = 1 + b 1 (* recursive function has to have a variable *)
-and 
-	b (c) = 2 + 1
+	a c = 1 + b 1 (* recursive function has to have a variable *)
+and
+	b c = a (2 + 1)
 
-(* let rec 
-	a (c) = lazy (1 + b 1)
-and 
-	b (c) = 2 + 1
- *)
+let rec 
+	a' c = lazy (1 + b' 1)
+and
+	b' c = force (a') (2 + 1)
+
+(* a 3 *)
 
 (* 
 
