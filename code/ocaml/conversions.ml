797bc3b1d22e036ddfd65b0cb2acb4a68db124bc
Rok Krulec <rok.krulec@gmail.com>
2015-01-06 18:23:31 -0500

    OCaml matching examples

--- code/ocaml/conversions.ml
+++ code/ocaml/conversions.ml
@@ -7,3 +7,5 @@ Int.of_string (String.of_char_list digits))
 Pervasives.int_of_string
 Pervasives.string_of_int
 
+| List ((Atom "if") :: predicate :: consequence :: alternative :: _) ->
+| List [Atom "if"; predicate; consequence; alternative] ->
