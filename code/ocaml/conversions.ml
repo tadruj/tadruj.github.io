4171b28752bbbf72549e7214b3d6c0577fef56f4
Rok Krulec <rok.krulec@gmail.com>
2015-01-07 22:41:42 -0500

    Cleaning up the files

--- code/ocaml/conversions.ml
+++ code/ocaml/conversions.ml
@@ -7,5 +7,6 @@ Int.of_string (String.of_char_list digits))
 Pervasives.int_of_string
 Pervasives.string_of_int
 
+(* List Matching *)
 | List ((Atom "if") :: predicate :: consequence :: alternative :: _) ->
 | List [Atom "if"; predicate; consequence; alternative] ->
