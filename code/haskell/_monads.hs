bf5ee25b796d8bbc54b8c1374a3f5e192cbd213a
Rok Krulec <rok.krulec@gmail.com>
2015-01-16 17:23:55 -0500

    Haskell Monads head for ghci

---
+++ code/haskell/_monads.hs
@@ -0,0 +1,9 @@
+import Control.Monad
+import Data.Functor
+import Control.Applicative
+import Control.Monad.Trans -- liftIO
+import Text.Read
+import Data.Maybe
+import Control.Arrow
+import System.Random
+import Control.Monad.State
