c4da16d2299d72ab7695c7acbbdaf41b3d12e454
Rok Krulec <rok.krulec@gmail.com>
2015-02-07 00:10:37 -0500

    CLJS GrepOM Ankha and Draggable integration

--- code/clojurescript/grepom/project.clj
+++ code/clojurescript/grepom/project.clj
@@ -7,7 +7,8 @@
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.omcljs/om "0.8.8"]
                  [sablono "0.3.1"]
-                 [ankha "0.1.5.1-21e6ac"]]
+                 [ankha "0.1.5.1-21e6ac"]
+                 [ff-om-draggable "0.0.18"]]
 
   :plugins [[lein-cljsbuild "1.0.4"]]
 
--- code/clojurescript/grepom/src/grepom/core.cljs
+++ code/clojurescript/grepom/src/grepom/core.cljs
@@ -2,11 +2,16 @@
   (:require [om.core :as om]
             [clojure.string :as string]
             [ankha.core :as ankha]
-            [sablono.core :as html :refer-macros [html]]))
+            [sablono.core :as html :refer-macros [html]]
+            [grepom.components.results :as c-result]
+            [ff-om-draggable.core :as ff]))
 
 (enable-console-print!)
 
-(def app-state (atom {:docs [] :text "Hello world!" :example {:greeting "Aloha" :count 0}}))
+(def app-state (atom {:docs [] 
+                      :text "Hello world!" 
+                      :example {:greeting "Aloha" :count 0} 
+                      :position {:left 500 :top 20}}))
 
 (defn example [payload owner opts]
 (reify 
@@ -19,45 +24,31 @@
   (will-mount [_]
               (print "example will-mount"))))
 
-(defn strip-filename [grep-line]
-  (when-let [filtered-line (re-find #"[-:].*" grep-line)]
-    (subs filtered-line 1)))
-
-(defn result-line [payload owner opts]
-  (om/component
-    (let [line (strip-filename payload)] 
-    (html [:div line])))
-  )
-
-(defn result-item [payload owner opts]
-  (om/component
-    (let [lines (->> (:results payload)
-                      string/split-lines
-                      (remove string/blank?) ;; (comp not string/empty?)
-                      (take 6))]
-	    (html [:div 
-	           [:div "Query:" (get-in payload [:request :query])]
-	           [:div "Result:" (om/build-all result-line lines)]]))))
-
-(defn result-list [payload owner opts]
-  (om/component 
-    ;;(html [:div (map #(om/build result-item %) payload)])))
-    (html [:div (om/build-all result-item payload)])))
-
 (om/root
   (fn [app owner]
     (reify om/IRender
       (render [_]
-        (html [:div [:h1 (:text app)]
-               (om/build example (:example app))
-               (om/build result-list (:docs app))              
-               [:div (om/build ankha/inspector app)]]))))
+	      (let [docs (:docs app)
+	            sorted-docs (->> docs
+	                             (sort-by :time)
+	                             reverse)] 
+	        (html [:div [:h1 (:text app)]
+	               (om/build example (:example app))
+	               (om/build c-result/result-list sorted-docs)              
+	               [:div (om/build (ff/draggable-item ankha/inspector [:position]) app)]])))))
   app-state
   {:target (. js/document (getElementById "app"))})
 
 (def firebase-app-url "https://hackerscool.firebaseio.com/")
 (def r (js/Firebase. (str firebase-app-url "grep/search/response")))
 (.on r "child_added" (fn [data] 
-                       (let [blob (js->clj (.val data) :keywordize-keys true)]
+                       (let [blob (merge {:key (.key data)} 
+                                         (js->clj (.val data) :keywordize-keys true))]
                          (swap! app-state update-in [:docs] conj blob)
                          )))
+(.on r "child_removed" (fn [data] 
+                       (let [blob (js->clj (.val data) :keywordize-keys true)]
+                         (js/console.log (.key data))
+						 ;; (swap! app-state update-in [:docs] (fn [docs] (remove #(= deleted-key (:key %)) docs))                         
+						 (swap! app-state update-in [:docs] (fn [docs] (vec (remove #(= (.key data) (:key %)) docs)))))))
+
